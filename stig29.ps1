736ef7309d94b0e844392b309d998c182fe224b1
Noah Grayson <124320422+noah-sec@users.noreply.github.com>
2025-05-11 15:22:34 -0700

    Create stig29.ps1

---
+++ stig29.ps1
@@ -0,0 +1,19 @@
+#Requires -RunAsAdministrator
+# Defining the registry path and registry value:
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization\"
+$valueName = "NoLockScreenCamera"
+$valueData = 1
+
+# Checking for registry path, if none then creating path:
+if (-not (Test-Path $registryPath)) {
+    Write-Host "Registry path '$registryPath' not found. Creating it now."
+    New-Item -Path $registryPath -Force
+} else {
+    Write-Host "Registry path '$registryPath' already exists."
+}
+
+# Setting value:
+Set-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -Type DWord
+
+# Setting output message:
+Write-Host "Registry value '$valueName' set to '$valueData' at '$registryPath'."
